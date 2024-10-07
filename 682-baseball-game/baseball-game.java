7602b5a829d80018d8e699f2ff0940bef2fe9078
Ruvaid Ahmad <118987852+Ruvaidahmad123@users.noreply.github.com>
2024-10-08 00:08:54 +0530

    Time: 2 ms (93.56%) | Memory: 41.4 MB (82.83%) - LeetSync

--- 682-baseball-game/baseball-game.java
+++ 682-baseball-game/baseball-game.java
@@ -1,44 +1,24 @@
 class Solution {
-    public static boolean isInteger(String str) {
-        if (str == null) {
-            return false;
-        }
-        try {
-            Integer.parseInt(str);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
     public int calPoints(String[] operations) {
-        Stack<Integer>st=new Stack<>();
-        for(int i=0;i<operations.length;i++){
-            if(isInteger(operations[i])){
-                st.push(Integer.parseInt(operations[i]));
-            }
-            else if(operations[i].equals("D")){
-                int peek=st.peek();
-                st.push(peek*2);
-            }
-            else if(operations[i].equals("C")){
-                st.pop();
-            }
-            else{
-                int peek1=0;
-                int peek2=0;
-                if(!st.isEmpty())
-                peek1=st.pop();
-                if(!st.isEmpty())
-                peek2=st.pop();
-                st.push(peek2);
-                st.push(peek1);
-                st.push(peek1+peek2);
+        Stack<Integer> sample=new Stack<>();
+        for(String operation : operations) {
+            if(operation.equals("D")) {
+                sample.push(sample.peek()*2);
+            } else if(operation.equals("C")) {
+                sample.pop();
+            }else if(operation.equals("+")) {
+                Integer temp = sample.pop();
+                Integer last= sample.peek();
+                sample.push(temp);
+                sample.push(last+temp);
+            } else {
+                sample.push(Integer.valueOf(operation));
             }
         }
-        int ans=0;
-        while(!st.isEmpty()){
-            ans+=st.pop();
+        int sum = 0;
+        while(!sample.isEmpty()) {
+            sum += sample.pop();
         }
-        return ans;
+        return sum;
     }
 }
