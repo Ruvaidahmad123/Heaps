fc3e6195746f5b15eb9b002bba48e4a103eb31c9
Ruvaid Ahmad <118987852+Ruvaidahmad123@users.noreply.github.com>
2024-10-09 21:38:49 +0530

    Create merge_k_sortedArrays.java

---
+++ merge_k_sortedArrays.java
@@ -0,0 +1,18 @@
+class Solution
+{
+    //Function to merge k sorted arrays.
+    public static ArrayList<Integer> mergeKArrays(int[][] arr,int K) 
+    {
+        PriorityQueue<Integer>pq=new PriorityQueue<>();
+        for(int i=0;i<K;i++){
+            for(int j=0;j<K;j++){
+                pq.add(arr[i][j]);
+            }
+        }
+        ArrayList<Integer>ans=new ArrayList<>();
+        while(!pq.isEmpty()){
+            ans.add(pq.poll());
+        }
+        return ans;
+    }
+}
