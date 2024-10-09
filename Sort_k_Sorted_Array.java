50cd1f1743babf56532fd74361eb46d1da51a41a
Ruvaid Ahmad <118987852+Ruvaidahmad123@users.noreply.github.com>
2024-10-09 21:51:17 +0530

    Create Sort_k_Sorted_Array.java

---
+++ Sort_k_Sorted_Array.java
@@ -0,0 +1,20 @@
+class Solution
+{
+    //Function to return the sorted array.
+    ArrayList <Integer> nearlySorted(int arr[], int num, int k)
+    {
+        // your code here
+        ArrayList<Integer> list = new ArrayList<>();
+        PriorityQueue<Integer> q = new PriorityQueue<Integer>();
+        for(int i = 0 ;i<arr.length;i++){
+            q.offer(arr[i]);
+            if(q.size()>k){
+                list.add(q.poll());
+            }
+        }
+        while(!q.isEmpty()){
+            list.add(q.poll());
+        }
+        return list;
+    }
+}
