de6f9e1a290660d8c52557d703e0fd84418126bc
Ruvaid Ahmad <118987852+Ruvaidahmad123@users.noreply.github.com>
2024-10-10 21:40:01 +0530

    Time: 11 ms (42.40%) | Memory: 45.6 MB (86.10%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,32 +1,49 @@
-class Pair{
-    int diff;
-    int val;
-    Pair(int diff,int val){
-        this.diff=diff;
-        this.val=val;
-    }
-}
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        PriorityQueue<Pair>pq=new PriorityQueue<>((a,b)->{
-            if(a.diff!=b.diff){
-                return Integer.compare(b.diff,a.diff);
+        List<Integer>ans=new ArrayList<>();
+        int i=findfloor(arr,x);
+        int j=i+1;
+        while(j<arr.length && i>=0 && k>0){
+            if(Math.abs(x-arr[i])<=Math.abs(x-arr[j])){
+                ans.add(arr[i]);
+                i--;
             }
             else{
-                return Integer.compare(b.val,a.val);
+                ans.add(arr[j]);
+                j++;
             }
-        });
-        for(int i=0;i<arr.length;i++){
-            pq.add(new Pair(Math.abs(arr[i]-x),arr[i]));
-            if(pq.size()>k){
-                pq.poll();
-            }
+            k--;
         }
-        List<Integer>ans=new ArrayList<>();
-        while(!pq.isEmpty()){
-            ans.add(pq.poll().val);
+        while(j<arr.length && k>0){
+            ans.add(arr[j]);
+            j++;
+            k--;
+        }
+        while(i>=0 && k>0){
+            ans.add(arr[i]);
+            i--;
+            k--;
         }
         Collections.sort(ans);
+        return ans;
+    }
+    public int findfloor(int arr[],int x){
+        int low=0;
+        int high=arr.length-1;
+        int ans=0;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==x){
+                return mid;
+            }
+            if(arr[mid]<x){
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
         return ans;
     }
 }
