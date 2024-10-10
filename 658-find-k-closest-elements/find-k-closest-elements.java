f8f6ad2d755ff4ed2cd17882546a82aef5bc4468
Ruvaid Ahmad <118987852+Ruvaidahmad123@users.noreply.github.com>
2024-10-10 21:44:39 +0530

    Update find-k-closest-elements.java

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,3 +1,37 @@
+class Pair{
+    int diff;
+    int val;
+    Pair(int diff,int val){
+        this.diff=diff;
+        this.val=val;
+    }
+}
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        PriorityQueue<Pair>pq=new PriorityQueue<>((a,b)->{
+            if(a.diff!=b.diff){
+                return Integer.compare(b.diff,a.diff);
+            }
+            else{
+                return Integer.compare(b.val,a.val);
+            }
+        });
+        for(int i=0;i<arr.length;i++){
+            pq.add(new Pair(Math.abs(arr[i]-x),arr[i]));
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        List<Integer>ans=new ArrayList<>();
+        while(!pq.isEmpty()){
+            ans.add(pq.poll().val);
+        }
+        Collections.sort(ans);
+        return ans;
+    }
+}
+
+
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         List<Integer>ans=new ArrayList<>();
@@ -45,5 +79,27 @@ class Solution {
             }
         }
         return ans;
+    }
+}
+
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int left = 0, right = arr.length - k;
+        
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (x - arr[mid] > arr[mid + k] - x) {   //mid starting of window    
+                                                        //mid+k first index after ending or window
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        
+        List<Integer> result = new ArrayList<>();
+        for (int i = left; i < left + k; i++) {
+            result.add(arr[i]);
+        }
+        return result;
     }
 }
